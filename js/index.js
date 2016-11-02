8234cb3ac1fe1fa6dd07fed9e4d7055f7cb78647
lanbingyoujing <981012996@qq.com>
2016-11-02 17:55:55 +0800

    瀑布流布局

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 $(function(){
+
+    //跳转到瀑布流布局
+    $('.waterfall').click(function () {
+        window.open('https://lanbingyoujing.github.io/test/html/waterfall/waterfall.html');
+    });
+
+    //跳转到炫酷相册
     $('.supper-photo').click(function () {
-        window.location.href='https://lanbingyoujing.github.io/test/html/supperPhoto/supperphoto.html'
-    })
+        window.open('https://lanbingyoujing.github.io/test/html/supperPhoto/supperphoto.html');
+    });
 });
 
 
